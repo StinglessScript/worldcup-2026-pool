3baae075ae0688346069034666d1a6308ca1f1d5
Congvo <congvh@longvan.net>
2026-05-31 22:50:44 +0700

    Fix sidebar leaderboard layout for proper scrolling

--- web/src/components/features/Sidebar.tsx
+++ web/src/components/features/Sidebar.tsx
@@ -123,9 +123,9 @@ export const Sidebar = () => {
           ))}
         </nav>
 
-        {/* Leaderboard - scrollable */}
+        {/* Leaderboard - scrollable with fade */}
         <div className="flex-1 min-h-0 flex flex-col border-t border-white/10 overflow-hidden">
-          <div className="flex-1 min-h-0 pt-2 pb-2">
+          <div className="relative flex-1 min-h-0">
             <LeaderboardList />
           </div>
         </div>
